ef6230136b87b2ac2bb42269b0a1bdbeec3f0ee6
Yaroslav Cherednikov <ycherednikov@gmail.com>
2024-06-22 22:30:02 +0300

    v2

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ export default function Item({ item, onDeleteItem, onToggleItem }) {
 
   return (
     <li>
-      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} checked={ item.packed ? true : false } />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
